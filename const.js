ad3280d69254229345cf846e0df59aa56bacdde5
sky-nemonemo <nobady928@yahoo.co.jp>
2025-02-11 17:02:57 +0900

    Update

--- const.js
+++ const.js
@@ -1,36 +1,37 @@
 // アイコン画像定数
 const baseUrl = "https://pjsekai.sega.jp/assets/img/special/dl/sns_icon/icon_unite";
-const iconmap = new Map()
-iconmap.set("一歌", "01_1_ichika");
-iconmap.set("咲希", "01_2_saki");
-iconmap.set("穂波", "01_3_honami");
-iconmap.set("志歩", "01_4_shiho");
-iconmap.set("Lミク", "01_5_miku");
-iconmap.set("Lルカ", "01_6_luka");
-iconmap.set("みのり", "02_1_minori");
-iconmap.set("遥", "02_2_haruka");
-iconmap.set("愛莉", "02_3_airi");
-iconmap.set("雫", "02_4_shizuku");
-iconmap.set("Mミク", "02_5_miku");
-iconmap.set("Mリン", "02_6_rin");
-iconmap.set("こはね", "03_1_kohane");
-iconmap.set("杏", "03_2_an");
-iconmap.set("彰人", "03_3_akito");
-iconmap.set("冬弥", "03_4_toya");
-iconmap.set("Vミク", "03_5_miku");
-iconmap.set("Vレン", "03_6_len");
-iconmap.set("Vメイコ", "03_7_meiko");
-iconmap.set("司", "04_1_tsukasa");
-iconmap.set("えむ", "04_2_emu");
-iconmap.set("寧々", "04_3_nene");
-iconmap.set("類", "04_4_rui");
-iconmap.set("Wミク", "04_5_miku");
-iconmap.set("Wカイト", "04_6_kaito");
-iconmap.set("奏", "05_1_kanade");
-iconmap.set("まふゆ", "05_2_mafuyu");
-iconmap.set("絵名", "05_3_ena");
-iconmap.set("瑞希", "05_4_mizuki");
-iconmap.set("Nミク", "05_5_miku");
+const iconmap = new Map([
+    ["一歌", "01_1_ichika"],
+    ["咲希", "01_2_saki"],
+    ["穂波", "01_3_honami"],
+    ["志歩", "01_4_shiho"],
+    ["Lミク", "01_5_miku"],
+    ["Lルカ", "01_6_luka"],
+    ["みのり", "02_1_minori"],
+    ["遥", "02_2_haruka"],
+    ["愛莉", "02_3_airi"],
+    ["雫", "02_4_shizuku"],
+    ["Mミク", "02_5_miku"],
+    ["Mリン", "02_6_rin"],
+    ["こはね", "03_1_kohane"],
+    ["杏", "03_2_an"],
+    ["彰人", "03_3_akito"],
+    ["冬弥", "03_4_toya"],
+    ["Vミク", "03_5_miku"],
+    ["Vレン", "03_6_len"],
+    ["Vメイコ", "03_7_meiko"],
+    ["司", "04_1_tsukasa"],
+    ["えむ", "04_2_emu"],
+    ["寧々", "04_3_nene"],
+    ["類", "04_4_rui"],
+    ["Wミク", "04_5_miku"],
+    ["Wカイト", "04_6_kaito"],
+    ["奏", "05_1_kanade"],
+    ["まふゆ", "05_2_mafuyu"],
+    ["絵名", "05_3_ena"],
+    ["瑞希", "05_4_mizuki"],
+    ["Nミク", "05_5_miku"],
+]);
 
 // カテゴリ用定数
 const categories = new Set([
@@ -49,4 +50,13 @@ const series = new Set([
     "天文学者の研究室", "きらめく流星ルーム", "かがやくクローバールーム", "鮮やかなユニゾンルーム", "はじけるクラウンルーム",
     "ひび割れたハートルーム", "はじまりのメロディルーム", "月が見える旅館", "旅人のキャンプ",
     "Leo/need", "MORE MORE JUMP！", "Vivid BAD SQUAD", "ワンダーランズ×ショウタイム", "25時、ナイトコードで。", "バーチャル・シンガー"
+]);
+
+// ユニット識別子用定数
+const units = new Map([
+    ["L", "L/n"],
+    ["M", "MMJ"],
+    ["V", "VBS"],
+    ["W", "W×S"],
+    ["N", "N25"],
 ]);
